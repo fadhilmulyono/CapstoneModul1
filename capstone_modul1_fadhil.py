ce904304ba17c3565baa97311ec72a6a9717ad53
Fadhil Mulyono <fadhilmulyono@gmail.com>
2023-02-17 18:21:08 +0700

    Update Function Read Data and add comments

--- capstone_modul1_fadhil.py
+++ capstone_modul1_fadhil.py
@@ -1,3 +1,4 @@
+# Dictionary untuk List Data Penjualan Toko
 products = {
     1: {'nama': 'Bola lampu', 'harga': 10000, 'jumlah': 70},
     2: {'nama': 'Meteran', 'harga': 20000, 'jumlah': 73},
@@ -7,11 +8,12 @@ products = {
     6: {'nama': 'Gitar', 'harga': 19500, 'jumlah': 98},
     7: {'nama': 'Kelereng', 'harga': 3000, 'jumlah': 89},
     8: {'nama': 'Stapler', 'harga': 22000, 'jumlah': 42},
-    9: {'nama': 'Alat perkakas', 'harga': 6500, 'jumlah': 70}
+    9: {'nama': 'Alat perkakas', 'harga': 65000, 'jumlah': 20}
 }
 
 
 def main():
+    # Menampilkan Menu Opsi Program - Capstone Project
     while True:
         menu_option = int(input('''
     Welcome to Capstone Emmerce by Fadhil Mulyono Yulius
@@ -25,20 +27,27 @@ def main():
     Input Menu Option : '''))
 
         if menu_option == 1:
+            # Fungsi Read Data
             read_data()
         elif menu_option == 2:
+            # Fungsi Create Data
             create_data()
         elif menu_option == 3:
+            # Fungsi Update Data
             update_data()
         elif menu_option == 4:
+            # Fungsi Delete Data
             delete_data()
         elif menu_option == 5:
+            # Keluar dari program
             break
         else:
+            # Munculkan error jika salah input menu
             print("\n\tMohon maaf, input menu yang Anda masukkan salah.")
 
 
 def create_data():
+    # Tampil Menu Menambahkan Data (Tambah Data)
     while True:
         menu_option = int(input('''
     Menambahkan Data Penjualan Toko
@@ -48,39 +57,52 @@ def create_data():
     Input Menu Option : '''))
 
         if menu_option == 1:
+            # User input Primary Key (ID) data yang akan ditambahkan
             id_tambah = int(input('''
     Tambah Data Penjualan
     Masukkan ID data yang ingin ditambahkan: '''))
 
+            # Cek input data duplikat
             if id_tambah in products:
                 print("\n\tMohon maaf, ID data yang Anda masukkan ({}) sudah ada.".format(id_tambah))
             else:
-                # Initialize empty dictionary for new data
+                # Initialize Dictionary untuk data yang akan ditambahkan
                 products[id_tambah] = {}
 
+                # User input kelengkapan data (Nama, Harga, Jumlah barang)
                 print("\n\tTambah Data Penjualan")
                 nama_barang = input("\tMasukkan nama barang: ")
                 harga_barang = int(input("\tMasukkan harga barang: "))
                 jumlah_barang = int(input("\tMasukkan jumlah barang terjual: "))
 
-                simpan_data = input("\tYakin menambahkan data yang baru Anda masukkan ({})? ")
+                # Menampilkan Menu pilihan Simpan Data
+                simpan_data = input("\tYakin menambahkan data yang baru Anda masukkan untuk ID : {} ? "
+                                    .format(id_tambah))
 
                 if simpan_data.lower() == "ya":
+                    # Input "ya" untuk Simpan Data
                     print("\n\tSukses, data berhasil ditambahkan.")
-                    products.update({id_tambah: {nama_barang, harga_barang, jumlah_barang}})
+                    products[id_tambah]['nama'] = nama_barang
+                    products[id_tambah]['harga'] = harga_barang
+                    products[id_tambah]['jumlah'] = jumlah_barang
                 else:
+                    # Jika input selain "ya" atau dibiarkan kosong, data tidak tersimpan
                     print("\n\tMohon maaf, data tidak berhasil ditambahkan.")
 
         elif menu_option == 2:
+            # Kembali ke menu utama
             break
         else:
+            # Munculkan error jika salah input menu
             print("\n\tMohon maaf, input menu yang Anda masukkan salah.")
 
 
 def read_data():
-    border = '=' * 40
+    # Initialize variable border untuk output tabel
+    border = '=' * 60
 
     while True:
+        # Tampil Menu Menampilkan Data (Baca Data)
         menu_option = int(input('''
     Menampilkan Data Penjualan Toko
     1. Tampilkan Seluruh Data
@@ -90,43 +112,71 @@ def read_data():
     Input Menu Option : '''))
 
         if menu_option == 1:
-            print("""
+            if len(products) == 0:
+                # Tampilkan notifikasi Tidak Ada Data
+                print("\n\tMohon maaf, tidak ada data penjualan toko yang dapat ditampilkan.")
+            else:
+                # Initialize variable Total Penjualan Produk per kolom
+                total_penjualan = 0
+
+                # Menampilkan Seluruh Data
+                print("""
     Menampilkan Seluruh Data Penjualan Toko
     {}
-    {} | {:<15} | {:<5} | {}
-    {}""".format(border, "No", "Nama Produk", "Harga", "Jumlah", border))
-            for i in products:
-                # display
-                print("""\t {} | {:<15} | {:<5} | {}""".format
-                      (i, products[i]['nama'], products[i]['harga'], products[i]['jumlah']))
-            print("\t{}".format(border))
+    {} | {:<15} | {:<10} | {:<6} | {}
+    {}""".format(border, "No", "Nama Produk", "Harga", "Jumlah", "Total", border))
+                for i in products:
+                    # Total Penjualan = Kalikan harga produk dengan jumlah produk terjual
+                    total = products[i]['harga'] * products[i]['jumlah']
+
+                    # Jumlahkan semua kolom Total Penjualan
+                    total_penjualan += total
+
+                    # Tampilkan daftar penjualan per kolom
+                    print("""\t {} | {:<15} | Rp. {:<6} | {:<6} | Rp. {}""".format
+                          (i, products[i]['nama'], products[i]['harga'], products[i]['jumlah'], total))
+
+                # Tampilkan hasil dari penjumlahan Total Penjualan
+                print("""\t{}
+         Total Penjualan : Rp. {}""".format(border, total_penjualan))
 
         elif menu_option == 2:
+            # User input Primary Key (ID) data yang ingin dicari
             id_cari = int(input('''
     Cari Data Berdasarkan ID
     Masukkan ID data yang ingin dicari: '''))
 
+            # Cek apakah ID data yang dicari ada di Dictionary
             if id_cari not in products.keys():
+                # Tampilkan notifikasi Tidak Ada Data
                 print("\n\tMohon maaf, ID data yang Anda masukkan ({}) tidak ada.".format(id_cari))
             else:
+                # Total Penjualan = Kalikan harga produk dengan jumlah produk terjual
+                total = products[id_cari]['harga'] * products[id_cari]['jumlah']
 
+                # Menampilkan Data Sesuai Input User
                 print("""
     Menampilkan Data Penjualan Berdasarkan ID: {}
     {}
-    Nama Produk: {}
-    Harga Produk: Rp. {}
-    Jumlah Penjualan Produk: {}
-    {}"""
-                      .format(id_cari, border, products[id_cari]['nama'], products[id_cari]['harga'],
-                              products[id_cari]['jumlah'], border))
+    {} | {:<15} | {:<10} | {:<6} | {}
+    {}
+     {} | {:<15} | Rp. {:<6} | {:<6} | Rp. {}
+    {}""".format(id_cari, border, "No", "Nama Produk", "Harga", "Jumlah", "Total", border, id_cari,
+                 products[id_cari]['nama'], products[id_cari]['harga'], products[id_cari]['jumlah'], total, border))
 
         elif menu_option == 3:
+            # Kembali ke menu utama
             break
         else:
+            # Munculkan error jika salah input menu
             print("\n\tMohon maaf, input menu yang Anda masukkan salah.")
 
 
 def update_data():
+    # Initialize variable border untuk output tabel
+    border = '=' * 60
+
+    # Tampil Menu Mengubah Data (Edit Data)
     while True:
         menu_option = int(input('''
     Mengubah Data Penjualan Toko
@@ -136,19 +186,36 @@ def update_data():
     Input Menu Option : '''))
 
         if menu_option == 1:
+            # User input Primary Key (ID) data yang ingin diubah
             id_update = int(input('''
     Ubah Data Penjualan
     Masukkan ID data yang ingin diubah: '''))
 
             if id_update not in products.keys():
+                # Tampilkan notifikasi Tidak Ada Data
                 print("\n\tMohon maaf, ID data yang Anda masukkan ({}) tidak ada.".format(id_update))
             else:
-                print("\n\tUbah Data Penjualan")
+                # Total Penjualan = Kalikan harga produk dengan jumlah produk terjual
+                total = products[id_update]['harga'] * products[id_update]['jumlah']
+
+                # Menampilkan Data Sesuai ID Data yang diinput user
+                print("""
+    Menampilkan Data Penjualan Berdasarkan ID: {}
+    {}
+    {} | {:<15} | {:<10} | {:<6} | {}
+    {}
+     {} | {:<15} | Rp. {:<6} | {:<6} | Rp. {}
+    {}""".format(id_update, border, "No", "Nama Produk", "Harga", "Jumlah", "Total", border, id_update,
+                 products[id_update]['nama'], products[id_update]['harga'], products[id_update]['jumlah'],
+                 total, border))
+
+                print("\n\tUbah Data Penjualan Untuk ID: {}".format(id_update))
+                # User input kolom yang akan di Update (Nama, Harga, Jumlah barang)
                 nama_barang = input("\tMasukkan nama barang: ")
                 harga_barang = int(input("\tMasukkan harga barang: "))
                 jumlah_barang = int(input("\tMasukkan jumlah barang terjual: "))
 
-                simpan_data = input("\tYakin menyimpan perubahan data yang Anda masukkan untuk ID: {} ? "
+                simpan_data = input("\tYakin menyimpan perubahan data yang Anda masukkan untuk ID : {} ? "
                                     .format(id_update))
 
                 if simpan_data.lower() == "ya":
@@ -160,12 +227,15 @@ def update_data():
                     print("\n\tMohon maaf, data tidak berhasil diubah.")
 
         elif menu_option == 2:
+            # Kembali ke menu utama
             break
         else:
+            # Munculkan error jika salah input menu
             print("\n\tMohon maaf, input menu yang Anda masukkan salah.")
 
 
 def delete_data():
+    # Tampil Menu Menghapus Data (Delete Data)
     while True:
         menu_option = int(input('''
     Menghapus Data Penjualan Toko
@@ -175,24 +245,31 @@ def delete_data():
     Input Menu Option : '''))
 
         if menu_option == 1:
+            # User input Primary Key (ID) data yang ingin dihapus
             id_delete = int(input('''
     Hapus Data Penjualan
     Masukkan ID data yang ingin dihapus: '''))
 
             if id_delete not in products.keys():
+                # Tampilkan notifikasi Tidak Ada Data
                 print("\n\tMohon maaf, ID data yang Anda masukkan ({}) tidak ada.".format(id_delete))
             else:
-                hapus_data = input("\n\tYakin menghapus data yang sesuai dengan ID: {} ? ".format(id_delete))
+                # Menampilkan Menu pilihan Hapus Data
+                hapus_data = input("\n\tYakin menghapus data yang sesuai dengan ID : {} ? ".format(id_delete))
 
                 if hapus_data.lower() == "ya":
+                    # Input "ya" untuk Menghapus Data
                     print("\n\tSukses, data berhasil dihapus.")
                     del products[id_delete]
                 else:
+                    # Jika input selain "ya" atau dibiarkan kosong, data tidak dihapus
                     print("\n\tMohon maaf, data tidak berhasil dihapus.")
 
         elif menu_option == 2:
+            # Kembali ke menu utama
             break
         else:
+            # Munculkan error jika salah input menu
             print("\n\tMohon maaf, input menu yang Anda masukkan salah.")
 
 
